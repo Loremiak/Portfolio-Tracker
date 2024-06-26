97d53134662035ef0733e7024d3785918927a2cc
Damian Jamróz <mrlorem@gmail.com>
2024-06-26 18:30:05 +0200

    build

--- src/helpers/formatNumber.tsx
+++ src/helpers/formatNumber.tsx
@@ -1,16 +1,13 @@
-export function formatNumber(num) {
-	const suffixes = ['', 'tys.', 'mln', 'mld', 'bln']; // Sufiksy dla kolejnych wielkości
+export function formatNumber(num: number) {
+	const suffixes = ['', 'tys.', 'mln', 'mld', 'bln'];
 
-	// Dzielenie liczby przez tysiące, aż będzie mniejsza niż tysiąc lub dojdzie do ostatniego sufiksu
 	let i = 0;
 	while (num >= 1000 && i < suffixes.length - 1) {
 		num /= 1000;
 		i++;
 	}
 
-	// Zaokrąglenie liczby do dwóch miejsc po przecinku
 	num = Math.round(num * 100) / 100;
 
-	// Zwrócenie liczby z odpowiednim sufiksem
 	return num.toString() + ' ' + suffixes[i];
 }

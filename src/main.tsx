bc82b14faefe9686d0ae43788992fda577ca2083
Damian Jamróz <mrlorem@gmail.com>
2024-07-02 23:40:11 +0200

    .

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
-import { HashRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<HashRouter>
+		<BrowserRouter>
 			<QueryClientProvider client={queryClient}>
 				<Provider store={store}>
 					<App />
 				</Provider>
 			</QueryClientProvider>
-		</HashRouter>
+		</BrowserRouter>
 	</React.StrictMode>
 );

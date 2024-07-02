8e2967d5641fbf38c58d60258d9bd84cdf1d426a
Damian Jamróz <mrlorem@gmail.com>
2024-07-02 12:47:24 +0200

    add api key

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -17,7 +17,9 @@ export function useCryptocurrenciesList() {
 	return useQuery({
 		queryKey: ['cryptocurrenciesList'],
 		queryFn: async () => {
-			const response = await axios.get<Coins>(`${BASE_URL}/coins/markets?vs_currency=usd&per_page=10&page=1`);
+			const response = await axios.get<Coins>(
+				`${BASE_URL}/coins/markets?vs_currency=usd&per_page=10&page=1&x_cg_demo_api_key=${apiKey}`
+			);
 
 			return response.data;
 		},
@@ -41,7 +43,7 @@ export function useTrendingCoins() {
 	return useQuery({
 		queryKey: ['trendingCoins'],
 		queryFn: async () => {
-			const response = await axios.get<TrendingCoinsData>(`${BASE_URL}/search/trending`);
+			const response = await axios.get<TrendingCoinsData>(`${BASE_URL}/search/trending&x_cg_demo_api_key=${apiKey}`);
 			return response.data.coins;
 		},
 	});
@@ -51,7 +53,7 @@ export function useGlobalMarketData() {
 	return useQuery({
 		queryKey: ['globalMarketData'],
 		queryFn: async () => {
-			const response = await axios.get<DataMarket>(`${BASE_URL}/global`);
+			const response = await axios.get<DataMarket>(`${BASE_URL}/global&x_cg_demo_api_key=${apiKey}`);
 			return response.data.data;
 		},
 	});
@@ -61,7 +63,7 @@ export function useCoinDetailsInfo(id: string) {
 	return useQuery({
 		queryKey: ['coinDetailsInfo', id],
 		queryFn: async () => {
-			const response = await axios.get<CoinDetails>(`${BASE_URL}/coins/${id}`);
+			const response = await axios.get<CoinDetails>(`${BASE_URL}/coins/${id}&x_cg_demo_api_key=${apiKey}`);
 			return response.data;
 		},
 	});
@@ -71,7 +73,9 @@ export function useHistoricalChartData(id: string) {
 	return useQuery({
 		queryKey: ['historicalData', id],
 		queryFn: async () => {
-			const response = await axios.get<ChartData>(`${BASE_URL}/coins/${id}/market_chart?vs_currency=usd&days=1`);
+			const response = await axios.get<ChartData>(
+				`${BASE_URL}/coins/${id}/market_chart?vs_currency=usd&days=1&x_cg_demo_api_key=${apiKey}`
+			);
 			return response.data;
 		},
 	});
